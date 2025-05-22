f86620cffdae4b86c69f8be9452516a6af78d411
Tarsis Lima <br.tmvdl@gmail.com>
2025-05-21 22:26:42 -0300

    Initialize package.json for sap-node-api with express dependency

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ datetime: new Date().toISOString() })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
